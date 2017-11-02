ffd961f05e9aa61ba2233bc603338a370dc25540
paulvercoustre <paulvercoustre@gmail.com>
2017-11-02 01:59:55 +0100

    Finished feature extraction

--- scripts/data_manager.py
+++ scripts/data_manager.py
@@ -4,14 +4,14 @@ import numpy as np
 import pandas as pd
 import psycopg2
 
-
-ROOT = os.path.join(os.path.dirname(__file__), '../')
 FLAGS = None
+ROOT = os.path.join(os.path.dirname(__file__), '../')
+
+config_file = os.path.join(ROOT, 'config/config.json')
+pickle_file = os.path.join(ROOT, 'cache', 'telemetry.npy')
 
 
 def main():
-
-    config_file = os.path.join(ROOT, 'config/config.json')
 
     with open(config_file) as conf:
         config = json.load(conf)
@@ -24,36 +24,146 @@ def main():
                                 password=conn_config['password'],
                                 dbname=conn_config['dbname'])
 
-        telemetry = getTelemetry(conn)
+        # get and pickle raw data
+        get_telemetry(conn)
+
+        extractor = FeatureLabelExtractor(pickle_file)
+        X = extractor.get_sequential_features
+
+        for col in X.columns.values:
+            print(sum(pd.isnull(X[col])))
+            print(X[col])
 
     finally:
         conn.close()
 
 
-
-def getTelemetry(conn): 
-
+def get_telemetry(conn):
     curs = conn.cursor()
 
-    query = '''
+    query = """
     SELECT created_at as date, data as telemetry, model, os, osv, real_pump
     FROM total_telemetry_data
-    '''
+    """
 
     curs.execute(query)
     data = list(curs.fetchall())
 
     np.save(os.path.join(ROOT, 'cache', 'telemetry.npy'), data)
-    data = data[0]
-    print(data[0])
-    print(data[1][0])
-    print(data[1][1])
-
-    # idfas = np.asarray(list(map(lambda x: x[:][0], data)))
 
     curs.close()
 
-    return
+
+class FeatureLabelExtractor:
+
+    """
+    Get the features (sequential or not) and labels from pickle.
+
+    Parameters:
+    ----------
+    pickle_file: the .npy file to be unpacked
+
+
+    Attributes:
+    ----------
+
+
+    """
+
+    def __init__(self, pickle_file):
+
+        self._raw_data = np.load(pickle_file)
+        self._raw_telemetry = []
+        self._telemetry = {'8594C654-6565-4DBC-9FA6-BEC41B929609_1_1': [],
+                          '8594C654-6565-4DBC-9FA6-BEC41B929609_1_2': [],
+                          '8594C654-6565-4DBC-9FA6-BEC41B929609_2_1': [],
+                          '8594C654-6565-4DBC-9FA6-BEC41B929609_2_2': [],
+                          '8594C654-6565-4DBC-9FA6-BEC41B929609_3_1': [],
+                          '8594C654-6565-4DBC-9FA6-BEC41B929609_3_2': [],
+                          '8594C654-6565-4DBC-9FA6-BEC41B929609_3_3': [],
+                          '1192EF5B-6CA3-4ACA-B632-C00BC1CC703C_1_1': [],
+                          '1192EF5B-6CA3-4ACA-B632-C00BC1CC703C_1_2': [],
+                          '1192EF5B-6CA3-4ACA-B632-C00BC1CC703C_2_1': [],
+                          '1192EF5B-6CA3-4ACA-B632-C00BC1CC703C_2_2': [],
+                          '1192EF5B-6CA3-4ACA-B632-C00BC1CC703C_3_1': [],
+                          '1192EF5B-6CA3-4ACA-B632-C00BC1CC703C_3_2': [],
+                          '1192EF5B-6CA3-4ACA-B632-C00BC1CC703C_3_3': [],
+                          'F74B56F7-5F12-413B-BF5E-DF09CC7E5C33_1_1': [],
+                          'F74B56F7-5F12-413B-BF5E-DF09CC7E5C33_1_2': [],
+                          'F74B56F7-5F12-413B-BF5E-DF09CC7E5C33_2_1': [],
+                          'F74B56F7-5F12-413B-BF5E-DF09CC7E5C33_2_2': [],
+                          'F74B56F7-5F12-413B-BF5E-DF09CC7E5C33_3_1': [],
+                          'F74B56F7-5F12-413B-BF5E-DF09CC7E5C33_3_2': [],
+                          'F74B56F7-5F12-413B-BF5E-DF09CC7E5C33_3_3': []}
+        self._sequential_features = []
+        self._features = []
+        self._label = []
+
+    @property
+    def get_labels(self):
+
+        self._label = np.asarray(list(map(lambda x: x[:][5], self._raw_data)))
+        return self._label
+
+    @property
+    def get_features(self):
+
+        dates = np.asarray(list(map(lambda x: x[:][0], self._raw_data)))
+        model = np.asarray(list(map(lambda x: x[:][2], self._raw_data)))
+        o_s = np.asarray(list(map(lambda x: x[:][3], self._raw_data)))
+        osv = np.asarray(list(map(lambda x: x[:][4], self._raw_data)))
+
+        self._features = pd.DataFrame(data={'dates': dates,
+                                            'model': model,
+                                            'os': o_s,
+                                            'os_version': osv})
+        return self._features
+
+    @property
+    def get_sequential_features(self):
+
+        # print(self.raw_data)
+        self._raw_telemetry = np.asarray(list(map(lambda x: x[:][1], self._raw_data)))
+
+        # for each test
+        for line in range(len(self._raw_telemetry)):
+
+            # get the list of detected beacons in the test
+            detected_beacons = []
+            for beacon in range(len(self._raw_telemetry[line])):
+                key = self.get_uuid_major_minor(line, beacon)
+                detected_beacons.append(key)
+
+            # for each possible beacon append  beacon data if detected
+            # otherwise append empty list
+            for beacon in self._telemetry.keys():
+
+                if beacon in detected_beacons:
+                    # find that beacon in raw_telemetry
+                    for detected_beacon in range(len(self._raw_telemetry[line])):
+
+                        key = self.get_uuid_major_minor(line, detected_beacon)
+                        if key == beacon:
+                            self._telemetry[beacon].append(self._raw_telemetry[line][detected_beacon]['raw_data'])
+                            break
+                        else:
+                            pass
+                else:
+                    # self._telemetry[beacon].append(np.nan)
+                    self._telemetry[beacon].append([])
+
+
+        # make Pandas DataFrame
+        self._sequential_features = pd.DataFrame(data=self._telemetry)
+        return self._sequential_features
+
+    def get_uuid_major_minor(self, line, beacon):
+
+        uuid = self._raw_telemetry[line][beacon]['beacon']['uuid']
+        major = self._raw_telemetry[line][beacon]['beacon']['major']
+        minor = self._raw_telemetry[line][beacon]['beacon']['minor']
+        key = uuid + '_' + str(major) + '_' + str(minor)
+        return key
 
 
 if __name__ == '__main__':
--- scripts/sequencial_model.py
+++ scripts/sequencial_model.py
@@ -2,34 +2,74 @@ import os
 import numpy as np
 import pandas as pd
 
-# from keras.models import Sequential
-# from keras.layers import Dense, Activation
+import data_manager as dm
 
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import RNN, SimpleRNN, Dense, Activation, Dropout
+
+FLAGS = None
 ROOT = os.path.join(os.path.dirname(__file__), '../')
+
 file_name = os.path.join(ROOT, 'data', 'no_filter.csv')
+pickle_file = os.path.join(ROOT, 'cache', 'telemetry.npy')
 
 
 def main():
-    data = np.genfromtxt(fname=file_name,
-                         delimiter=",",
-                         skip_header=1,
-                         usecols=range(1, 24),
-                         filling_values=-120)
 
-    # create features and labels
-    X = data[:, 2:24]  # each feature is the signal intensity of a given beacon at a given time step
-    y = data[:, 0]
+    # import the data
+    # data = np.genfromtxt(fname=file_name,
+    #                      delimiter=",",
+    #                      skip_header=1,
+    #                      usecols=range(1, 24),
+    #                      filling_values=-120)
+    #
+    # # create features and labels
+    # X = data[:, 2:24]  # each feature is the signal intensity of a given beacon at a given time step
+    # y = data[:, 0]
+    #
+    # n_samples, n_features = X.shape
+    #
+    # X = X.reshape(n_samples, 7, 3)
+    # X = X.transpose(0, 2, 1)
 
-    n_samples, n_features = X.shape
+    extractor = dm.FeatureLabelExtractor(pickle_file)
+    X = extractor.get_sequential_features
+    y = extractor.get_labels
 
-    X = X.reshape(n_samples, 7, 3)
-    X = X.transpose(0, 2, 1)
-    print(X.shape)
+    # pad the telemetry to 4 timesteps, fill with -120 empty data
+    X = pad_dataframe(X, 4, -120)
+
+    n_samples, timesteps, m_features = X.shape
+
+    print('\nBuilding Model...')
+    model = Sequential()
+    model.add(SimpleRNN(8, return_sequences=True, input_shape=(timesteps, m_features)))
+    model.add(Dropout(0.9))
+    model.add(SimpleRNN(8))
+    model.add(Dense(8))
+    model.add(Activation('softmax'))
+    print('Model Built')
+
+    print('\nCompiling Model...')
+    model.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    print(model.summary())
 
 
-# model = Sequential()
-# model.add(RNN(10, ))
+def pad_dataframe(dataframe, maxlen, value):
 
+    padded = []
+    for col in dataframe.columns.values:
+        padded.append(pad_sequences(sequences=dataframe[col],
+                                    maxlen=maxlen,
+                                    padding='pre',
+                                    truncating='pre',
+                                    value=value))
+
+    padded = np.asarray(padded).transpose(1, 2, 0)
+    return padded
 
 if __name__ == '__main__':
     main()
